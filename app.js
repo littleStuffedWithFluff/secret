a3917df91584f17a0ba25fb6deaddc46e5c59e7c
littleStuffedWithFluff <161811358+littleStuffedWithFluff@users.noreply.github.com>
2024-03-11 15:14:19 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const express = require('express');
+/*const express = require('express');
 const bodyParser = require('body-parser');
 const request = require('request');
 const https = require('https');
@@ -23,6 +23,34 @@ app.post('/', (req, res) => {
   request.end();
 });
 
-app.listen(8080, () => {
-  console.log(`Example app listening on port 8080`)
-});
+app.listen(3000, () => {
+  console.log(`Example app listening on port 3000`)
+});*/
+
+
+const express = require('express')
+
+const app = express()
+const port = 3030
+const bodyParser = require('body-parser')
+const oauthServer = require('./oauth/server.js')
+
+const DebugControl = require('./utilities/debug.js')
+
+//Here we are configuring express to use body-parser as middle-ware.
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+app.use(DebugControl.log.request())
+
+app.use('/client', require('./routes/client.js')) // Client routes
+app.use('/oauth', require('./routes/auth.js')) // routes to access the auth stuff
+// Note that the next router uses middleware. That protects all routes within this middleware
+app.use('/secure', (req,res,next) => {
+  DebugControl.log.flow('Authentication')
+  return next()
+},oauthServer.authenticate(), require('./routes/secure.js')) // routes to access the protected stuff
+app.use('/', (req,res) => res.redirect('/client'))
+
+
+app.listen(port)
+console.log("Oauth Server listening on port ", port)
